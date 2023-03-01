fe14e222fec1d1ac9cd49856818aa0a29409421d
prabhu88 <s.prabhu.mtech88@gmail.com>
2023-03-01 16:28:26 +0530

    Todo search - completed

--- src/components/headers/topnav.js
+++ src/components/headers/topnav.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {NavLink} from 'react-router-dom'
 import './topnav.css'
 const TopNav = () => {
+    const [searchText,setSearchText] = useState('')
     return(
         <header className="sticky-top blog-header lh-1 py-3 ">
             <div className="row flex-nowrap justify-content-between align-items-center">
@@ -10,11 +12,18 @@ const TopNav = () => {
                 <div className="col-4 text-center">
                     <a className="blog-header-logo text-dark" href="#">Task Mate</a>
                 </div>
-                <div className="col-4 d-flex justify-content-end align-items-center">
-                    <a className="link-secondary" href="#" aria-label="Search">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+                <div className="col-4 d-flex justify-content-end align-items-center masterDiv">
+                    <input type="text" className="inputSearch" onChange={(e)=>{
+                        setSearchText(e.target.value)
+                    }} placeholder="Enter text to search todo's" />
+                    <a className="link-secondary Search-Icon" href="#" aria-label="Search">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
                     </a>
-                    <a className="btn btn-sm btn-outline-secondary" href="#">Search</a>
+                    {/* <a className="btn btn-sm btn-outline-secondary" href="#">Search</a> */}
+                    <NavLink to={'/todo/search/'+searchText}
+                        className="btn btn-sm btn-outline-secondary"
+                    >Search
+                    </NavLink>
                 </div>
             </div>
         </header>
